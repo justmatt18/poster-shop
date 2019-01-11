8502946c9dc8412b27c842ea5c7fc03b81efc665
staff01 hubport <staff01-hubport@hpi.local>
2019-01-11 12:17:53 +0800

    add logics to the cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,35 @@
-console.log("It works!");
+new Vue ({
+    el: '#app',
+    data() {
+        return {
+            total: 0,
+            products: [
+                { title: "Product 1" , id: 1, price: 5},
+                { title: "Product 2" , id: 2, price: 10},
+                { title: "Product 3" , id: 3, price: 15},
+            ],
+            cart: [],
+        }
+    },
+    methods: {
+        addToCart: function(product){
+            var found = false;
+            this.total += product.price;
+            for (var i = 0; i < this.cart.length; i++){
+                if(this.cart[i].id === product.id){
+                    found = true;
+                    this.cart[i].qty++;
+                }
+            }
+            if(!found){
+                this.cart.push({
+                    id: product.id,
+                    title: product.title,
+                    price: product.price,
+                    qty: 1,
+                });
+            }
+            
+        }
+    },
+})
