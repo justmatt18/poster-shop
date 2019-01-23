680b77f31e3289df563fa3c16894a50e3b1ace3f
staff01 hubport <staff01-hubport@hpi.local>
2019-01-23 17:36:09 +0800

    added ajax call from the server

--- public/script.js
+++ public/script.js
@@ -4,11 +4,13 @@ new Vue ({
         return {
             total: 0,
             products: [
-                { title: "Product 1" , id: 1, price: 5},
-                { title: "Product 2" , id: 2, price: 10},
-                { title: "Product 3" , id: 3, price: 15},
+                { title: "Product 1" , id: 1, price: 5.99},
+                { title: "Product 2" , id: 2, price: 10.99},
+                { title: "Product 3" , id: 3, price: 15.50},
             ],
             cart: [],
+            search: '',
+
         }
     },
     methods: {
@@ -30,6 +32,30 @@ new Vue ({
                 });
             }
             
+        },
+        inc: function(item){
+            item.qty++;
+            this.total += item.price;
+        },
+        dec: function(item){
+            item.qty--;
+            this.total -= item.price;
+            if(item.qty <= 0){
+                var i = this.cart.indexOf(item);
+                this.cart.splice(i, 1);
+            }
+        },
+        onSubmit: function(){
+            var path = "/search?q=".concat(this.search);
+            this.$http.get(path)
+                .then(function(response){
+                    console.log(response);
+                })
         }
     },
+    filters: {
+        currency: function(price){
+            return "$".concat(price.toFixed(2));
+        }
+    }
 })
